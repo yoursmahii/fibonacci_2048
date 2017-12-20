733a6df6f5a10be74ec25c270af7c992ac462a2a
mahendran <mahendran@Mahii.local>
2017-12-20 12:53:38 +0530

    Initializing ruby files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fibonacci2048::Application.config.secret_key_base = '02c2921d64138488ec75208d7a077628b27b63528238ca4d5662adb4f6033051c5e26802cd57c2ab24de448c9ada441edf6e1c78f81261e438dac5c134815ce3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fibonacci2048::Application.config.session_store :cookie_store, key: '_fibonacci_2048_session'
